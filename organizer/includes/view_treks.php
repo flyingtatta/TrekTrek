b77716f59fcec4ec4ba44e3bc0ecc233ed3c1456
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-14 18:33:22 +0530

    Made it a bit better by adding icons and ordering by the id

--- organizer/includes/view_treks.php
+++ organizer/includes/view_treks.php
@@ -4,7 +4,7 @@
 </h1>
 
 <?php
-  $query = "SELECT * FROM treks";
+  $query = "SELECT * FROM treks ORDER BY trek_id DESC";
   $select_treks = mysqli_query($connection, $query);
   confirmQuery($query);
 ?>
@@ -12,6 +12,8 @@
 
 <table class="table table-bordered">
   <thead>
+    <td>Delete</th>
+    <td>Edit</th>
     <th class="text-center">Id</th>
     <th class="text-center">Name</th>
     <th class="text-center px-3">Departure</th>
@@ -31,9 +33,10 @@
     while ($row = mysqli_fetch_assoc($select_treks)) {
       $trek_id        = $row['trek_id'];
       $trek_name      = $row['trek_name'];
-      $trek_departure = $row['trek_departure'];
-      $trek_arrival   = $row['trek_arrival'];
-      $trek_about     = $row['trek_about'];
+      $trek_departure = date("d-m-Y", strtotime($row['trek_departure']));
+      $trek_arrival   = date("d-m-Y", strtotime($row['trek_arrival']));
+      $trek_about     = trim(substr($row['trek_about'], 0, 50));
+      $trek_about     = $trek_about . "...";
       $trek_location  = $row['trek_location'];
       $trek_duration  = $row['trek_duration'];
       $trek_image     = $row['trek_image'];
@@ -44,11 +47,21 @@
       $trek_views     = $row['trek_views']; ?>
 
       <tr>
+        <td class="text-center">
+          <a href="treks.php?delete=<?php echo $trek_id; ?>">
+            <i class="fa fa-trash text-danger fa-lg"></i>
+          </a>
+        </td>
+        <td class="text-center">
+          <a href="">
+            <i class="fa fa-edit text-primary fa-lg"></i>
+          </a>
+        </td>
         <td class="text-center"><?php echo $trek_id; ?></td>
         <td class="text-center"><?php echo $trek_name; ?></td>
         <td class="text-center"><?php echo $trek_departure; ?></td>
         <td class="text-center"><?php echo $trek_arrival; ?></td>
-        <td class="text-justify"><?php echo $trek_about; ?></td>
+        <td class="text-left"><?php echo $trek_about; ?></td>
         <td class="text-center" ><?php echo $trek_location; ?></td>
         <td class="text-center" ><?php echo $trek_duration; ?></td>
         <td class="text-center" ><img src="trek-images/<?php echo $trek_image; ?>" width="200" height="100"></td>
@@ -56,9 +69,22 @@
         <td class="text-center" ><?php echo $trek_altitude; ?>ft</td>
         <td class="text-center" >&#8377;<?php echo $trek_price; ?></td>
         <td class="text-center"><?php echo $trek_status; ?></td>
-        <td class="text-center" ><?php echo $trek_views; ?></td>
-        <td class="text-center">
-          <a href="treks.php?delete=<?php echo $trek_id; ?>">Delete</a>
+        <td class="text-center" >
+          <span class="badge badge-primary badge-pill" style="background-color:
+          <?php if ($trek_views >= 0 && $trek_views < 25): ?>
+          <?php echo "#dc3545";?>
+          <?php endif; ?>
+
+          <?php if ($trek_views >= 25 && $trek_views < 50): ?>
+          <?php echo "#007bff"; ?>
+          <?php endif; ?>
+
+          <?php if ($trek_views >= 50): ?>
+          <?php echo "#28a745"; ?>
+          <?php endif; ?>
+          ;">
+            <?php echo $trek_views; ?>
+          </span>
         </td>
       </tr>
 
