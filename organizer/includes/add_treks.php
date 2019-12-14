3616b7e765958628ace6028c7f35c67ee9be2593
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-14 19:16:57 +0530

    calaculation of duration through departure and arrival. no need for manually adding type of altitude

--- organizer/includes/add_treks.php
+++ organizer/includes/add_treks.php
@@ -1,79 +1,80 @@
 <?php
-  if (isset($_POST['submit'])) {
-    $trek_name      = escape($_POST['trek_name']);
-    $trek_departure = escape($_POST['trek_departure']);
-    $trek_arrival   = escape($_POST['trek_arrival']);
-    $trek_about     = escape($_POST['trek_about']);
-    $trek_location  = escape($_POST['trek_location']);
-    $trek_duration  = escape($_POST['trek_duration']);
-    $trek_image     = $_FILES['image']['name'];
-    $tmp_trek_image = $_FILES['image']['tmp_name'];
-    $trek_type      = escape($_POST['trek_type']);
-    $trek_altitude  = escape($_POST['trek_altitude']);
-    $trek_price     = escape($_POST['trek_price']);
-    $trek_status    = 'On';
+if (isset($_POST['submit'])) {
+  $trek_name        = escape($_POST['trek_name']);
+  $trek_departure   = escape($_POST['trek_departure']);
+  $trek_arrival     = escape($_POST['trek_arrival']);
+  $trek_about       = escape($_POST['trek_about']);
+  $trek_location    = escape($_POST['trek_location']);
 
-    $error = [
-      'trek_name' => '',
-      'trek_departure' => '',
-      'trek_arrival' => '',
-      'trek_about' => '',
-      'trek_location' => '',
-      'trek_duration' => '',
-      'trek_altitude' => '',
-      'trek_price' => ''
-    ];
-
-    if (empty($_POST['trek_name'])) {
-      $error['trek_name'] = 'Trek Name cannot be empty';
-    }
-
-    if (empty($_POST['trek_departure'])) {
-      $error['trek_departure'] = 'Cannot be empty';
-    }
-    if (empty($_POST['trek_arrival'])) {
-      $error['trek_arrival'] = 'Cannot be empty';
-    }
-
-    if (empty($_POST['trek_about'])) {
-      $error['trek_about'] = 'Cannot be empty';
-    }
-    if (strlen($_POST['trek_about']) < 100) {
-      $error['trek_about'] = 'Atleast 30-35 words describing the place';
-    }
-
-    if (empty($_POST['trek_location'])) {
-      $error['trek_location'] = 'Trek Location cannot be empty';
-    }
-
-    if (empty($_POST['trek_duration'])) {
-      $error['trek_duration'] = 'Trek duration cannot be empty';
-    }
-
-    if ($_POST['trek_altitude'] < 0) {
-      $error['trek_altitude'] = 'Altitude cannot be in minus';
-    }
-
-    if (empty($_POST['trek_price'])) {
-      $error['trek_price'] = 'Trek Price cannot be empty';
-    }
-
-    if ($_POST['trek_price'] < 0) {
-      $error['trek_price'] = 'Enter a valid price';
-    }
-
-    foreach ($error as $key => $value) {
-      if (empty($value)) {
-        unset($error[$key]);
-      }
-    }
-
-    if (empty($error)) {
-      registerTrek($trek_name,$trek_departure,$trek_arrival,$trek_about,$trek_location,$trek_duration,$trek_image,$tmp_trek_image,$trek_type,$trek_altitude,$trek_price,$trek_status);
-    }
+  $dateDiff = strtotime($trek_departure) - strtotime($trek_arrival);
+  $trek_duration    =  abs(round($dateDiff/86400));
 
 
+  $trek_image       = $_FILES['image']['name'];
+  $tmp_trek_image   = $_FILES['image']['tmp_name'];
+  $trek_type        = escape($_POST['trek_type']);
+  $type_of_altitude = escape($_POST['type-of-altitude']);
+  $trek_altitude    = escape($_POST['trek_altitude']);
+  $trek_altitude    = $trek_altitude . $type_of_altitude;
+  $trek_price       = escape($_POST['trek_price']);
+  $trek_status      = 'On';
+
+  $error = [
+    'trek_name' => '',
+    'trek_departure' => '',
+    'trek_arrival' => '',
+    'trek_about' => '',
+    'trek_location' => '',
+    'trek_altitude' => '',
+    'trek_price' => ''
+  ];
+
+  if (empty($_POST['trek_name'])) {
+    $error['trek_name'] = 'Trek Name cannot be empty';
   }
+
+  if (empty($_POST['trek_departure'])) {
+    $error['trek_departure'] = 'Cannot be empty';
+  }
+  if (empty($_POST['trek_arrival'])) {
+    $error['trek_arrival'] = 'Cannot be empty';
+  }
+
+  if (empty($_POST['trek_about'])) {
+    $error['trek_about'] = 'Cannot be empty';
+  }
+  if (strlen($_POST['trek_about']) < 100) {
+    $error['trek_about'] = 'Atleast 30-35 words describing the place';
+  }
+
+  if (empty($_POST['trek_location'])) {
+    $error['trek_location'] = 'Trek Location cannot be empty';
+  }
+
+  if ($_POST['trek_altitude'] < 0) {
+    $error['trek_altitude'] = 'Altitude cannot be in minus';
+  }
+
+  if (empty($_POST['trek_price'])) {
+    $error['trek_price'] = 'Trek Price cannot be empty';
+  }
+
+  if ($_POST['trek_price'] < 0) {
+    $error['trek_price'] = 'Enter a valid price';
+  }
+
+  foreach ($error as $key => $value) {
+    if (empty($value)) {
+      unset($error[$key]);
+    }
+  }
+
+  if (empty($error)) {
+    registerTrek($trek_name,$trek_departure,$trek_arrival,$trek_about,$trek_location,$trek_duration,$trek_image,$tmp_trek_image,$trek_type,$trek_altitude,$trek_price,$trek_status);
+  }
+
+
+}
 ?>
 <h1 class="display-4 text-center">
   Add Trek
@@ -98,7 +99,7 @@
 
     <div class="row">
       <div class="col-12 col-sm-6 form-group">
-        <label for="">Trek departue:</label>
+        <label for="">Trek Departure:</label>
         <input type="date" name="trek_departure" class="form-control"
         value="<?php if(isset($_POST['submit'])){ echo $_POST['trek_departure']; } ?>">
         <p class="text-danger">
@@ -111,7 +112,7 @@
       </div>
 
       <div class="col-12 col-sm-6 form-group">
-        <label for="">Trek arrival:</label>
+        <label for="">Trek Arrival:</label>
         <input type="date" name="trek_arrival" class="form-control"
         value="<?php if(isset($_POST['submit'])){ echo $_POST['trek_arrival']; } ?>">
         <p class="text-danger">
@@ -155,15 +156,6 @@
 
     <div class="row">
       <div class="col-12 col-sm-6 form-group">
-        <label for="">No. of Days:</label>
-        <select class="form-control" name="trek_duration">
-          <?php for ($i=1; $i <= 7; $i++) { ?>
-            <option value="<?php echo $i; ?>"><?php echo $i; ?> day<?php if ($i>=2){echo "s";} ?></option>
-          <?php } ?>
-        </select>
-      </div>
-
-      <div class="col-12 col-sm-6 form-group">
         <label for="">Type of Trek:</label>
         <select class="form-control" name="trek_type">
           <?php if (isset($_POST['submit'])) { ?>
@@ -174,18 +166,28 @@
           <option value="camping">Camping Trek</option>
         </select>
       </div>
-    </div>
 
-    <div class="row">
       <div class="col-12 col-sm-6 form-group">
         <label for="">Image:</label> <br>
         <input type="file" name="image">
       </div>
 
+    </div>
+
+    <div class="row">
+
       <div class="col-12 col-sm-6 form-group">
         <label for="">Altitude (in Ft):</label>
-        <input type="number" name="trek_altitude" class="form-control"
-        value="<?php if(isset($_POST['submit'])){ echo $_POST['trek_altitude']; } ?>">
+        <div class="input-group">
+          <input type="number" name="trek_altitude" class="form-control" value="<?php if(isset($_POST['submit'])){ echo $_POST['trek_altitude']; } ?>">
+          <div class="input-group-append">
+            <select class="form-control" name="type-of-altitude">
+              <option value="ft">ft</option>
+              <option value="m">m</option>
+            </select>
+          </div>
+        </div>
+
         <p class="text-danger">
           <?php
           if (isset($error['trek_altitude'])) {
@@ -194,13 +196,16 @@
           ?>
         </p>
       </div>
-    </div>
 
-    <div class="row">
-      <div class="col-12 col-sm-12 form-group">
+      <div class="col-12 col-sm-6 form-group">
         <label for="">Trek Price:</label>
-        <input type="number" name="trek_price" class="form-control"
-        value="<?php if(isset($_POST['submit'])){ echo $_POST['trek_price']; } ?>">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">&#8377;</span>
+          </div>
+          <input type="number" name="trek_price" class="form-control"
+          value="<?php if(isset($_POST['submit'])){ echo $_POST['trek_price']; } ?>">
+        </div>
         <p class="text-danger">
           <?php
           if (isset($error['trek_price'])) {
