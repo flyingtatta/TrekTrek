e034d8be2c60ba7e65230e661f66eaf333b6a840
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-15 21:59:52 +0530

    created a function for update query

--- organizer/includes/organizer_functions.php
+++ organizer/includes/organizer_functions.php
@@ -12,4 +12,24 @@ function registerTrek($trek_name,$trek_departure,$trek_arrival,$trek_about,$trek
   confirmQuery($stmt);
   header("Location: treks.php");
 }
+
+function updateTrek($trek_id,$trek_name,$trek_departure,$trek_arrival,$trek_about,$trek_location,$trek_duration,$trek_image,$tmp_trek_image,$trek_type,$trek_altitude,$trek_price,$trek_status){
+
+  global $connection;
+
+  if (empty($trek_image)) {
+    $query = mysqli_query($connection, "SELECT trek_image FROM treks WHERE trek_id = $trek_id");
+    confirmQuery($query);
+    while($row = mysqli_fetch_assoc($query)){
+      $trek_image = $row['trek_image'];
+    }
+  }
+  move_uploaded_file($tmp_trek_image, "trek-images/$trek_image");
+
+  $stmt = mysqli_prepare($connection, "UPDATE treks SET trek_name = ?, trek_departure = ?, trek_arrival = ?,trek_about = ?, trek_location = ?,trek_duration = ?,trek_image = ?,trek_type = ?,trek_altitude = ?,trek_price = ?,trek_status = ? WHERE trek_id = ?;");
+  mysqli_stmt_bind_param($stmt, "sssssssssssi", $trek_name,$trek_departure,$trek_arrival,$trek_about,$trek_location,$trek_duration,$trek_image,$trek_type,$trek_altitude,$trek_price,$trek_status,$trek_id);
+  mysqli_stmt_execute($stmt);
+  confirmQuery($stmt);
+  header("Location: treks.php");
+}
 ?>
