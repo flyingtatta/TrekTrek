04c88dcd5cdde70492fbc0566501dbeee1079666
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-20 19:33:58 +0530

    Refactored code using functions and dropdowns are now links because some error in making admin

--- includes/navigation.php
+++ includes/navigation.php
@@ -19,44 +19,51 @@ if (isset($_POST['submit'])) {
           <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
         </li>
 
-        <?php if (!isset($_SESSION['username'])): ?>
-          <li class="nav-item">
-            <a class="nav-link" href="register.php">Register</a>
-          </li>
-        <?php endif; ?>
-
-        <?php if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'Organizer'): ?>
-
-          <li class="nav-item">
-            <a class="nav-link" href="organizer">Organizer</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="organizer/treks.php">Treks</a>
-          </li>
-
-        <?php endif; ?>
+      <?php if (isLoggedout()): ?>
+        <li class="nav-item">
+          <a class="nav-link" href="register.php">Register</a>
+        </li>
+      <?php endif; ?>
 
 
-        <?php if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'Admin'): ?>
-          <li class="nav-item">
-            <a class="nav-link" href="admin">Admin</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="admin/users.php">Users</a>
-          </li>
-        <?php endif; ?>
+      <?php if (isOrganizer()): ?>
+        <li class="nav-item">
+          <a class="nav-link" href="organizer">Organizer</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="organizer/treks.php">Treks</a>
+        </li>
+
+      <?php elseif (isAdmin()): ?>
+
+        <li class="nav-item">
+          <a class="nav-link" href="admin">Admin</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Users
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="admin/users.php?source=view_all_users">View All Users</a>
+            <a class="dropdown-item" href="admin/users.php?source=add_users">Add User</a>
+          </div>
+        </li>
+
+      <?php endif; ?>
 
       </ul>
 
+      <?php if (isLoggedout()): ?>
 
-
-      <?php if (!isset($_SESSION['user_email'])): ?>
         <form class="form-inline" action="" method="post">
           <input class="form-control mr-2" type="text"     placeholder="Email"    name="user_email" >
           <input class="form-control mr-2" type="password" placeholder="Password" name="user_password" >
           <input class="form-control mr-2 btn btn-success" type="submit" name="submit" value="Login">
         </form>
+
       <?php else: ?>
+
         <ul class="nav nav-pills mr-2">
           <li class="nav-item dropdown active">
             <a class="nav-link text-light text-decoration-none dropdown-toggle" href='' id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -64,17 +71,19 @@ if (isset($_POST['submit'])) {
               <?php echo $_SESSION['user_firstname'] . ". " . $_SESSION['user_lastname']; ?>
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="../profile.php">Profile</a>
+              <a class="dropdown-item" href="./profile.php">Profile</a>
               <a class="dropdown-item" href="./includes/logout.php">Logout</a>
             </div>
           </li>
         </ul>
+
       <?php endif; ?>
 
       <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
       </form>
+
     </div>
   </nav>
 </div>
