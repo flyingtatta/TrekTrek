cf865a4569416cfb950798ffd3de5cb7a47c09a8
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-23 23:53:44 +0530

    IIgnoree the commented part

--- index.php
+++ index.php
@@ -1,32 +1,34 @@
-<?php include 'includes/header.php'; ?>
-<?php session_start(); ?>
-<?php include 'includes/functions.php'; ?>
-<?php include 'includes/navigation.php'; ?>
-
-
-
-<?php include 'includes/main.php'; ?>
+<?php
+  include 'includes/header.php';
+  session_start();
+  include 'includes/functions.php';
+  include 'includes/navigation.php';
+  include 'includes/main.php';
+?>
 
 <!-- <?php
-  if (isset($_SESSION['user_role'])) {
-    if ($_SESSION['user_role'] == 'Organizer') {
-      $trek_username = $_SESSION['username'];
-      $trek_user_email = $_SESSION['user_email'];
-      $trek_user_phonenumber = $_SESSION['user_phonenumber'];
 
-      $query = mysqli_query($connection,"SELECT * FROM trek_organizer WHERE trek_user_email = '$trek_user_email'");
-      $select_organizer = mysqli_execute($query);
-      $row = mysqli_fetch_assoc($select_organizer);
-      echo $row;
-      if ($row >= 1) {
+  //UNDEFINED THING THAT STOPPED THE ORGANIZERS AND EVERYONES DROP DOWN TO STOP WORKING
 
-      }else{
-        $query = mysqli_query($connection, "INSERT INTO trek_organizer(trek_organizer_name, trek_organizer_email, trek_organizer_phonenumber) VALUES('$trek_username', '$trek_user_email', '$trek_user_phonenumber')");
-        mysqli_execute($query);
-        confirmQuery($query);
-      }
-    }
-  }
+  // if (isset($_SESSION['user_role'])) {
+  //   if ($_SESSION['user_role'] == 'Organizer') {
+  //     $trek_username = $_SESSION['username'];
+  //     $trek_user_email = $_SESSION['user_email'];
+  //     $trek_user_phonenumber = $_SESSION['user_phonenumber'];
+  //
+  //     $query = mysqli_query($connection,"SELECT * FROM trek_organizer WHERE trek_user_email = '$trek_user_email'");
+  //     $select_organizer = mysqli_execute($query);
+  //     $row = mysqli_fetch_assoc($select_organizer);
+  //     echo $row;
+  //     if ($row >= 1) {
+  //
+  //     }else{
+  //       $query = mysqli_query($connection, "INSERT INTO trek_organizer(trek_organizer_name, trek_organizer_email, trek_organizer_phonenumber) VALUES('$trek_username', '$trek_user_email', '$trek_user_phonenumber')");
+  //       mysqli_execute($query);
+  //       confirmQuery($query);
+  //     }
+  //   }
+  // }
 ?> -->
 
 <?php include 'includes/footer.php'; ?>
