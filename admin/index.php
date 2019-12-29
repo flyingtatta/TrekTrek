5ff107a9aff115ca7501572308fbe17f09983ed3
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-29 17:49:56 +0530

    removed the making of charts and put it in a separate file

--- admin/index.php
+++ admin/index.php
@@ -3,53 +3,9 @@ include 'includes/admin_header.php';
 session_start();
 
 if (isAdmin()){
+  
   include 'includes/admin_navigation.php';
-
-  $accounts = recordCount('users');
-  //tablename, where data, where clause
-  $users = recordCountFor('users', 'user_role','User');
-  $organizers = recordCountFor('users', 'user_role','Organizer');
-  $admin = recordCountFor('users', 'user_role','Admin');
-  $treks = recordCount('treks');
-
-?>
-
-
-  <div class="container mt-5">
-    <script type="text/javascript">
-    google.charts.load('current', {'packages':['bar']});
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ['', 'Count'],
-
-        <?php
-
-        $element_text = ['Accounts','Users','Organizers', 'Admin', 'Treks'];
-        $element_count = [$accounts,$users,$organizers,$admin,$treks];
-
-        for($i = 0; $i < sizeof($element_text); $i++){
-          echo "['{$element_text[$i]}'" . ", "."{$element_count[$i]}],";
-        }
-        ?>
-      ]);
-
-      var options = {
-        chart: {
-          title:    '',
-          subtitle: '',
-        }
-      };
-
-      var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-      chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-    </script>
-    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
-  </div>
-
-<?php
+  include 'includes/admin_charts.php';
   include 'includes/admin_footer.php';
 
 }else{
--- organizer/index.php
+++ organizer/index.php
@@ -3,52 +3,9 @@ include 'includes/organizer_header.php';
 session_start();
 
 if (isOrganizer()){
+  
   include 'includes/organizer_navigation.php';
-
-  $treks = recordCount('treks');
-  //tablename, where data, where clause
-
-  $treks_status_on  = recordCountFor('treks', 'trek_status', 'On');
-  $treks_status_off = recordCountFor('treks', 'trek_status', 'Off');
-?>
-
-
-  <div class="container mt-5">
-    <script type="text/javascript">
-    google.charts.load('current', {'packages':['bar']});
-    google.charts.setOnLoadCallback(drawChart);
-
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ['Date', 'Count'],
-
-        <?php
-
-        $element_text = ['Treks', 'Treks On', 'Treks Off'];
-        $element_count = [$treks, $treks_status_on, $treks_status_off];
-
-        for($i = 0; $i < sizeof($element_text); $i++){
-          echo "['{$element_text[$i]}'" . ", "."{$element_count[$i]}],";
-        }
-        ?>
-      ]);
-
-      var options = {
-        chart: {
-          title:    '',
-          subtitle: '',
-        }
-      };
-
-      var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-
-      chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-    </script>
-    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
-  </div>
-
-<?php
+  include 'includes/organizer_charts.php';
   include 'includes/organizer_footer.php';
 
 }else{
