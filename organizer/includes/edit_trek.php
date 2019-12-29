7f6f80d422224a2206f3ebbdc3dccc13d713c736
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-29 17:47:54 +0530

    Instead of trek_type changed it to trek_type_id

--- organizer/includes/edit_trek.php
+++ organizer/includes/edit_trek.php
@@ -82,25 +82,25 @@ if (isset($_GET['trek_id'])) {
 
   <form class="" action="" method="post" enctype="multipart/form-data">
     <?php
-      $query = "SELECT * FROM treks WHERE trek_id = $trek_id";
-      $select_trek = mysqli_query($connection, $query);
-      confirmQuery($select_trek);
-      while($row = mysqli_fetch_assoc($select_trek)){
-        $trek_name      = $row['trek_name'];
-        $trek_departure = $row['trek_departure'];
-        $trek_arrival   = $row['trek_arrival'];
-        $trek_about     = $row['trek_about'];
-        $trek_location  = $row['trek_location'];
-        $trek_duration  = $row['trek_duration'];
-        $trek_image     = $row['trek_image'];
-        $trek_type      = $row['trek_type'];
-        $trek_altitude  = $row['trek_altitude'];
-        $trek_altitude  = str_replace("ft", "", $trek_altitude);
-        $trek_altitude  = str_replace("m", "", $trek_altitude);
-        $trek_price     = $row['trek_price'];
-        $trek_status    = $row['trek_status'];
-        $trek_views     = $row['trek_views'];
-      }
+    $query = "SELECT * FROM treks WHERE trek_id = $trek_id";
+    $select_trek = mysqli_query($connection, $query);
+    confirmQuery($select_trek);
+    while($row = mysqli_fetch_assoc($select_trek)){
+      $trek_name      = $row['trek_name'];
+      $trek_departure = $row['trek_departure'];
+      $trek_arrival   = $row['trek_arrival'];
+      $trek_about     = $row['trek_about'];
+      $trek_location  = $row['trek_location'];
+      $trek_duration  = $row['trek_duration'];
+      $trek_image     = $row['trek_image'];
+      $trek_type_id   = $row['trek_type_id'];
+      $trek_altitude  = $row['trek_altitude'];
+      $trek_altitude  = str_replace("ft", "", $trek_altitude);
+      $trek_altitude  = str_replace("m", "", $trek_altitude);
+      $trek_price     = $row['trek_price'];
+      $trek_status    = $row['trek_status'];
+      $trek_views     = $row['trek_views'];
+    }
     ?>
     <div class="container mb-4">
       <div class="row">
@@ -167,128 +167,138 @@ if (isset($_GET['trek_id'])) {
         <div class="col-12 col-sm-12 form-group">
           <label for="">About the Trek:</label>
           <textarea name="trek_about" class="form-control" rows="6" cols="25" id="body"><?php
-            if(isset($_POST['submit'])){
-              echo $_POST['trek_about'];
-            }else{
-              echo $trek_about;
-            }?>
-          </textarea>
-          <p class="text-danger">
-            <?php
-            if (isset($error['trek_about'])) {
-              echo $error['trek_about'];
-            }
-            ?>
-          </p>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-12 col-sm-12 form-group">
-          <label for="">Trek Location:</label>
-          <input type="text" name="trek_location" class="form-control"
-          value="<?php
           if(isset($_POST['submit'])){
-            echo $_POST['trek_location'];
+            echo $_POST['trek_about'];
           }else{
-            echo $trek_location;
-          } ?>">
-          <p class="text-danger">
-            <?php
-            if (isset($error['trek_location'])) {
-              echo $error['trek_location'];
-            }
+            echo $trek_about;
+          }?>
+        </textarea>
+        <p class="text-danger">
+          <?php
+          if (isset($error['trek_about'])) {
+            echo $error['trek_about'];
+          }
+          ?>
+        </p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 col-sm-12 form-group">
+        <label for="">Trek Location:</label>
+        <input type="text" name="trek_location" class="form-control"
+        value="<?php
+        if(isset($_POST['submit'])){
+          echo $_POST['trek_location'];
+        }else{
+          echo $trek_location;
+        } ?>">
+        <p class="text-danger">
+          <?php
+          if (isset($error['trek_location'])) {
+            echo $error['trek_location'];
+          }
+          ?>
+        </p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12 col-sm-6 form-group">
+        <label for="">Type of Trek:</label>
+        <select class="form-control" name="trek_type_id">
+
+          <?php
+          $query = mysqli_query($connection, "SELECT * FROM trek_type WHERE trek_type_id = $trek_type_id");
+          confirmQuery($query);
+          $row = mysqli_fetch_assoc($query);
+          $trek_type_name = $row['trek_type_name'];
+          ?>
+
+          <option value="<?php echo $trek_type_id; ?>" selected><?php echo $trek_type_name; ?></option>
+
+          <?php
+          $query = mysqli_query($connection, "SELECT * FROM trek_type");
+          confirmQuery($query);
+
+          while($row = mysqli_fetch_assoc($query)){
+            $trek_type_id = $row['trek_type_id'];
+            $trek_type_name = $row['trek_type_name'];
             ?>
-          </p>
-        </div>
+
+            <option value="<?php echo $trek_type_id; ?>"><?php echo $trek_type_name; ?></option>
+
+            <?php
+          }
+          ?>
+        </select>
       </div>
 
-      <div class="row">
-        <div class="col-12 col-sm-6 form-group">
-          <label for="">Type of Trek:</label>
-          <select class="form-control" name="trek_type_id">
-              <?php
-                $query = mysqli_query($connection, "SELECT * FROM trek_type");
-                confirmQuery($query);
-
-                while($row = mysqli_fetch_assoc($query)){
-                  $trek_type_id = $row['trek_type_id'];
-                  $trek_type_name = $row['trek_type_name'];
-              ?>
-
-              <option value="<?php echo $trek_type_id; ?>"><?php echo $trek_type_name; ?></option>
-
-              <?php
-                }
-              ?>
-          </select>
-        </div>
-
-        <div class="col-12 col-sm-6 form-group">
-          <label for="">Image:</label> <br>
-          <input type="file" name="image" value="<?php echo $trek_image; ?>">
-        </div>
-
-      </div>
-
-      <div class="row">
-
-        <div class="col-12 col-sm-6 form-group">
-          <label for="">Altitude:</label>
-          <div class="input-group">
-            <input type="number" name="trek_altitude" class="form-control" value="<?php
-            if(isset($_POST['submit'])){
-              echo $_POST['trek_altitude'];
-            }else{
-              echo $trek_altitude;
-            } ?>">
-            <div class="input-group-append">
-              <select class="form-control" name="type-of-altitude">
-                <option value="ft">ft</option>
-                <option value="m">m</option>
-              </select>
-            </div>
-          </div>
-
-          <p class="text-danger">
-            <?php
-            if (isset($error['trek_altitude'])) {
-              echo $error['trek_altitude'];
-            }
-            ?>
-          </p>
-        </div>
-
-        <div class="col-12 col-sm-6 form-group">
-          <label for="">Trek Price:</label>
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">&#8377;</span>
-            </div>
-            <input type="number" name="trek_price" class="form-control"
-            value="<?php
-            if(isset($_POST['submit'])){
-              echo $_POST['trek_price'];
-            }else {
-              echo $trek_price;
-            } ?>">
-          </div>
-          <p class="text-danger">
-            <?php
-            if (isset($error['trek_price'])) {
-              echo $error['trek_price'];
-            }
-            ?>
-          </p>
-        </div>
-      </div>
-
-      <div class="row justify-content-center">
-        <input type="submit" name="submit" value="Add This Trek" class="btn btn-outline-success">
+      <div class="col-12 col-sm-6 form-group">
+        <label for="">Image:</label> <br>
+        <input type="file" name="image" value="<?php echo $trek_image; ?>">
       </div>
 
     </div>
-  </form>
+
+    <div class="row">
+
+      <div class="col-12 col-sm-6 form-group">
+        <label for="">Altitude:</label>
+        <div class="input-group">
+          <input type="number" name="trek_altitude" class="form-control" value="<?php
+          if(isset($_POST['submit'])){
+            echo $_POST['trek_altitude'];
+          }else{
+            echo $trek_altitude;
+          } ?>">
+          <div class="input-group-append">
+            <select class="form-control" name="type-of-altitude">
+              <option value="ft">ft</option>
+              <option value="m">m</option>
+            </select>
+          </div>
+        </div>
+
+        <p class="text-danger">
+          <?php
+          if (isset($error['trek_altitude'])) {
+            echo $error['trek_altitude'];
+          }
+          ?>
+        </p>
+      </div>
+
+      <div class="col-12 col-sm-6 form-group">
+        <label for="">Trek Price:</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">&#8377;</span>
+          </div>
+          <input type="number" name="trek_price" class="form-control"
+          value="<?php
+          if(isset($_POST['submit'])){
+            echo $_POST['trek_price'];
+          }else {
+            echo $trek_price;
+          } ?>">
+        </div>
+        <p class="text-danger">
+          <?php
+          if (isset($error['trek_price'])) {
+            echo $error['trek_price'];
+          }
+          ?>
+        </p>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <input type="submit" name="submit" value="Add This Trek" class="btn btn-outline-success">
+    </div>
+
+  </div>
+</form>
 <?php
 }else{
   header("Location: ./treks.php");
