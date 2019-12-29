40058c566bda5b5a86636b8d0cfb37be495a7e4a
flyingtatta <47095611+flyingtatta@users.noreply.github.com>
2019-12-29 17:52:43 +0530

    font weight to  700

--- admin/includes/view_users.php
+++ admin/includes/view_users.php
@@ -4,25 +4,34 @@
 </h1>
 
 <?php
+if (!isset($_POST['search'])) {
   $query = "SELECT * FROM users ORDER BY user_id ASC";
   $select_users = mysqli_query($connection, $query);
   confirmQuery($query);
+}else if(isset($_POST['search'])){
+  $search_user = $_POST['search_tags'];
+  $query = "SELECT * FROM users WHERE user_role LIKE '%$search_user%' OR username LIKE '%$search_user%' OR user_firstname LIKE '%$search_user%' OR user_lastname LIKE '%$search_user%' OR user_email LIKE '%$search_user%' ORDER BY user_id ASC";
+  $select_users = mysqli_query($connection, $query);
+  confirmQuery($query);
+}
+
 ?>
 
 
 <table class="table table-bordered">
   <thead>
-    <td>Delete</th>
-    <th class="text-center">Id</th>
-    <th class="text-center">Firstname</th>
-    <th class="text-center">Lastname</th>
-    <th class="text-center">Username</th>
-    <th class="text-center">Email</th>
-    <th class="text-center">Phone</th>
-    <th class="text-center">Image</th>
-    <th class="text-center">DOB</th>
-    <th class="text-center">Role</th>
+    <td class="text-center" style="font-weight: 700;">Delete</th>
+    <th class="text-center" style="font-weight: 700;">Id</th>
+    <th class="text-center" style="font-weight: 700;">Firstname</th>
+    <th class="text-center" style="font-weight: 700;">Lastname</th>
+    <th class="text-center" style="font-weight: 700;">Username</th>
+    <th class="text-center" style="font-weight: 700;">Email</th>
+    <th class="text-center" style="font-weight: 700;">Phone</th>
+    <th class="text-center" style="font-weight: 700;">Image</th>
+    <th class="text-center" style="font-weight: 700;">DOB</th>
+    <th class="text-center" style="font-weight: 700;">Role</th>
   </thead>
+
   <tbody>
     <?php
     while ($row = mysqli_fetch_assoc($select_users)) {
@@ -38,9 +47,13 @@
     ?>
       <tr>
         <td class="text-center">
-          <a href="users.php?delete=<?php echo $user_id; ?>">
+          <form class="" action="" method="post" onsubmit="return confirm('Are you sure you want to delete?');">
+            <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+            <input type="submit" name="delete" value="Delete" class="btn btn-danger">
+          </form>
+          <!-- <a href="users.php?delete=<?php //echo $user_id; ?>">
             <i class="fa fa-trash text-danger fa-lg"></i>
-          </a>
+          </a> -->
         </td>
         <td class="text-center"><?php echo $user_id; ?></td>
         <td class="text-center"><?php echo $user_firstname; ?></td>
@@ -51,7 +64,9 @@
         <td class="text-center"><img src="../images/<?php echo $user_image; ?>" width="200" height="100"></td>
         <td class="text-center"><?php echo $user_dob; ?></td>
         <td class="text-center">
-          <a href="./users.php?roler_id=<?php echo $user_id; ?>&role=<?php echo $user_role; ?>">
+          <form class="" action="" method="post" onsubmit="return confirm('Are you sure you want to change?');">
+            <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+            <input type="hidden" name="user_role" value="<?php echo $user_role; ?>">
             <span class="badge badge-pill badge-danger" style="background-color:
               <?php if ($user_role == 'User'): ?>
                 <?php echo '#28a745'; ?>
@@ -61,9 +76,11 @@
                 <?php echo '#007bff'; ?>
                 <?php endif; ?>
               ;">
-            <?php echo $user_role; ?>
-          </span>
-          </a>
+
+              <input type="submit" name="edit" value="<?php echo $user_role; ?>" style="background: none; border: none; color: white; font-size: 0.8rem; outline:none;">
+            </span>
+          </form>
+
         </td>
       </tr>
 
@@ -77,14 +94,14 @@
 </div>
 
 <?php
-  if (isset($_GET['delete'])) {
-    $user_id = $_GET['delete'];
+  if (isset($_POST['delete'])) {
+    $user_id = $_POST['user_id'];
     deleteUser($user_id);
   }
 
-  if (isset($_GET['roler_id'])) {
-    $user_id = $_GET['roler_id'];
-    $user_role = $_GET['role'];
+  if (isset($_POST['edit'])) {
+    $user_id = $_POST['user_id'];
+    $user_role = $_POST['user_role'];
     changeRole($user_id, $user_role);
   }
 ?>
